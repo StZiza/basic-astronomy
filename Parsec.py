919fae5845a8dc2a449b8e923ddf328d7040eda3
azulaa <ecegdagdeviren@gmail.com>
2015-03-13 23:51:25 +0200

    Calculation of parsec
    This piece of code is a practice and it calculates the parsec value of a given Astronomical Unit. Returns the results in light years and AU.

---
+++ Parsec.py
@@ -0,0 +1,37 @@
+#http://www.codeskulptor.org/#user39_EdPuMNXkjm_0.py
+################################################
+################################################
+################################################	
+
+from math import *
+#uzaklık gir milyon km cinsinden parsek hesaplayacak ıy cinsinden
+
+b=input(' AU cinsinden uzaklik giriniz:')
+
+a=1./3600.
+#print a
+pc=float(b)/tan(radians(a))
+print tan(a)
+print float(b)
+print pc
+
+ly=3e5*365*24*60*60
+print ly
+au=150e6
+print au
+iy=(pc*au)/ly
+
+
+print 'Bulundugunuz gezegen icin 1 pc degeri:' 
+print pc, 'Astronomi birimi'
+print iy, 'Isik yili'
+
+
+#a - açı saniyesi
+#au - astronomi birimi
+#ıy - ışık yılı bilmem kaç km
+#sonuç - 
+################################################
+################################################
+################################################
+################################################
